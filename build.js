a5074cb8e2f46b2f0f21d8f39886db23c18d3650
Mojtaba Jafarian Abyaneh <mojtabajafarian13@gmail.com>
2026-09-07 17:37:29 -0400

    Container for layout, 40rem cap on sections for reading

--- build.js
+++ build.js
@@ -124,7 +124,7 @@ function masthead(base) {
 
   return `
     <header class="masthead">
-      <img class="avatar" src="${base}${D.profile.image}" alt="${attr(D.profile.name)}" width="200" height="300">
+      <img class="avatar" src="${base}${D.profile.image}" alt="${attr(D.profile.name)}" width="250" height="375">
       <div class="identity">
         <h1>${D.profile.name}</h1>
         <div class="subtitle">${D.profile.title}${
